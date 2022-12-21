b8a892e1bbd6bac26fead876d190951d904dd0d1
BenSeredga <112845645+BenSeredga@users.noreply.github.com>
2022-12-21 22:49:05 +0300

    там сервис появился ес че

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
 import HeaderSection from './components/HeaderSection';
+import ServiceSection from './components/ServiceSection';
 function App() {
   return (
    <>
     <HeaderSection />
+    <ServiceSection />
    </>
   );
 }
