034c8dcd87678af39d4e8560446457dbf6813f62
k1borG <kiborg66666666@yandex.ru>
2022-12-22 21:13:06 +0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.scss';
-
+import HeaderSection from './components/HeaderSection';
+import ServiceSection from './components/ServiceSection';
 function App() {
   return (
    <>
+    <HeaderSection />
+    <ServiceSection />
    </>
   );
 }
