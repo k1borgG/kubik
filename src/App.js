b5cf1858dee1a9ab6e368b89b2c374dd7c79ae89
BenSeredga <112845645+BenSeredga@users.noreply.github.com>
2022-12-22 22:45:34 +0300

    эксп добавил

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import HeaderSection from './components/HeaderSection';
 import ServiceSection from './components/ServiceSection';
+import ExpertiseSection from './components/ExpertiseSection';
 function App() {
   return (
    <>
     <HeaderSection />
     <ServiceSection />
+    <ExpertiseSection />
    </>
   );
 }
