36c3ebb07a46665644f8de6f8d67067cb6e60701
FUNNTUZY <74323730+FUNNTUZY@users.noreply.github.com>
2022-12-22 22:53:54 +0300

    уууууууууу

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import HeaderSection from './components/HeaderSection';
 import ServiceSection from './components/ServiceSection';
 import ExpertiseSection from './components/ExpertiseSection';
+import SupportSection from './components/SupportSection';
 function App() {
   return (
    <>
     <HeaderSection />
     <ServiceSection />
     <ExpertiseSection />
+    <SupportSection />
    </>
   );
 }
