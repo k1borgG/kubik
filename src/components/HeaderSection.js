da0dc4660a8af27a3ec1c8b78a2e770c0661f300
k1borG <kiborg66666666@yandex.ru>
2022-12-22 21:59:18 +0300

    menu

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import { Navbar } from './Navbar';
+import { Navbar } from './Navbar';
 import TopSection from '../components/TopSection';
 
 function HeaderSection() {
   return (
     <header className="HeaderSection" id="Home">
-      {/* <Navbar /> */}
+      <Navbar />
       <TopSection />
     </header>
   );
