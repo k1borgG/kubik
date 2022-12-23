a5051344a8c49e99ba4a4498522896c1ef9e3e7c
k1borG <kiborg66666666@yandex.ru>
2022-12-23 20:07:43 +0300

    base_fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.scss';
 import HeaderSection from './components/HeaderSection';
 import ServiceSection from './components/ServiceSection';
+import SupportSection from './components/SupportSection';
+import ExpertiseSection from './components/ExpertiseSection';
+import PlansSection from './components/PlansSection';
+
 function App() {
   return (
    <>
     <HeaderSection />
     <ServiceSection />
+    <SupportSection />
+    <ExpertiseSection />
+    <PlansSection />
    </>
   );
 }
