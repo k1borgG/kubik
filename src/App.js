f5470ce1d62261f09cb8fdb989b391c23bdd2d19
k1borG <kiborg66666666@yandex.ru>
2022-12-23 20:01:50 +0300

    It's BASE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import HeaderSection from './components/HeaderSection';
 import ServiceSection from './components/ServiceSection';
+import PlansSection from './components/PlansSection';
 function App() {
   return (
    <>
     <HeaderSection />
     <ServiceSection />
+    <PlansSection />
    </>
   );
 }
